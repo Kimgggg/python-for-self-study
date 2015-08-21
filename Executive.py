aba8d7a357173e26d2767d981811aaaf2e632715
吴祎楠 <wuyinan@crazyhornets.com>
2015-08-21 17:31:31 +0800

    求100以内素数,算法不明白

--- Executive.py
+++ Executive.py
@@ -8,10 +8,13 @@ import os
 
 #for key,value in SetDef.d.iteritems():
 #	print key,value
+import math
+def is_ss(s):
+    if s==1:
+        return False
+    for i in range(2,int(sqrt(s)+1)):
+        if s%i==0:
+            return False
+    return True
 
-print [d for d in os.listdir('.')]
-	
-	
-	
-
-
+print(filter(is_ss,range(1,100)))
