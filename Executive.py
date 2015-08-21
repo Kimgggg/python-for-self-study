319785c8b91611d0fd3e224c6e058b6a4bb7a4be
吴祎楠 <wuyinan@crazyhornets.com>
2015-08-21 13:56:07 +0800

    [a*b for a in range(1,10)]列表生成式

--- Executive.py
+++ Executive.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-import SetDef
-from collections import Iterable
+#import SetDef
+#from collections import Iterable
 
-for key,value in SetDef.d.iteritems():
-	print key,value
+#for key,value in SetDef.d.iteritems():
+#	print key,value
 
+b = 10
+print [a*b for a in range(1,10)]
+	
 
 
