ae090f1ea696a4528b6b06560754a594a8dabde5
吴祎楠 <wuyinan@crazyhornets.com>
2015-08-25 10:26:50 +0800

    test PIL

--- Executive.py
+++ Executive.py
@@ -5,33 +5,9 @@
 #import SetDef
 from collections import Iterable
 import os
+import Image
 
-#for key,value in SetDef.d.iteritems():
-#	print key,value
-
-def lazy_sum(*args):
-    def sum():
-        ax = 0
-        for n in args:
-            ax = ax + n
-        return ax
-    return sum
-
-
-a=1
-b=2
-c=3
-
-print lazy_sum(a,b,c)
-
-print lazy_sum()
-
-f1 = lazy_sum(a,b,c)
-f2 = lazy_sum(a,b,c)
-
-print f1==f2
-
-print f1
-print f2
-
-#test github
+im = Image.open('/Users/wuyinan/Desktop/IMG_1026.JPG')
+w, h = im.size
+im.thumbnail((w//2, h//2))
+im.save('/Users/wuyinan/Desktop/IMG_10267.JPG')
