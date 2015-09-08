c74105a07f4ce3a9a9d60098c9268a03eb0f53b6
吴祎楠 <wuyinan@crazyhornets.com>
2015-09-08 11:10:34 +0800

    调试

---
+++ Classtest.py
@@ -0,0 +1,12 @@
+# py.py
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import types
+import functools
+
+class Student(object):
+	"""docstring for Student"""
+	pass
+
+
--- Executive.py
+++ Executive.py
@@ -4,13 +4,23 @@
 # still running on Python 2.7
 
 from collections import Iterable
+from types import MethodType
 import SetDef
-import SetClass
+import Classtest
 import os
 import Image
 import types
 
 
-print	dir(SetClass.Animal())
+s = Classtest.Student()
+s.name = raw_input('请输入姓名: \n')
+#print s.name
+
+def set_age(self, age):
+    self.age = age
 
 
+s.set_age = MethodType(set_age, s, Classtest.Student)
+s.set_age(input('shuru nianling:'))
+
+print s.age,s.name
