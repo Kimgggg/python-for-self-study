89ab861421be3f01d0dc2c90f66ed76909341940
吴祎楠 <wuyinan@crazyhornets.com>
2015-09-09 16:03:38 +0800

    斐波那契数列查询

--- Executive.py
+++ Executive.py
@@ -13,6 +13,28 @@ import Image
 import types
 
 
-s = SetClass.Dog()
+for n in SetClass.Fib():
+	pass
 
-s.run()
+f = SetClass.Fib()
+a = input('查询斐波那契数列\n 单独查询按“1”，多个查询按“2”: \n')
+
+if a != 1 and a != 2:
+	print 'Error'
+else:
+	if a == 2:
+		Number_Y = input('需要从第几个查询？\n') - 1
+		if Number_Y < 0:
+			print 'Error in Y<0'
+		Number_Z = input('需要查询到第几个?\n')  
+		if Number_Z < Number_Y:
+			print 'Error in Z<Y'
+		print '从第', Number_Y + 1, '个到 第', Number_Z, '个的斐波那契数是:\n',  f[Number_Y:Number_Z]
+	else:
+		Number_X = input('要查询第几个斐波那契数？：\n')  - 1
+		if Number_X < 0:
+			print 'Error'
+
+		else:
+			print '第',Number_X + 1,'个斐波那契数是:',f[Number_X]
+
--- SetClass.py
+++ SetClass.py
@@ -6,6 +6,12 @@ import types
 import functools
 
 class Student(object):
+	def __init__(self, name):
+		self.name = name
+	def __str__(self):
+		return 'Student object (name: %s)' % self.name
+	__repr__ = __str__
+
 	@property
 	def score(self):
 	    return self._score
@@ -43,7 +49,7 @@ class RunnableMixin(object):
 	def run(self):
 		print('Running...')
 
-class FylableMixin(object):
+class FylableMixi(object):
 	def fly(self):
 		print('Flying...')
 
@@ -56,23 +62,39 @@ class Dog(Mammal, RunnableMixin):
 class Bat(Mammal, RunnableMixin):
 	pass
 
-class Parrot(Bird, FylableMixin):
+class Parrot(Bird, FylableMixi):
 	pass
 
-class Ostrich(Bird, FylableMixin):
+class Ostrich(Bird, FylableMixi):
 	pass
 
-	
+class Fib(object):
+	def __init__(self):
+		self.a, self.b = 0, 1
 
-		
+	def __iter__(self):
+		return self
 
-		
+	def next(self):
+		self.a, self.b = self.b, self.a + self.b
+		if self.a > 2:
+			raise StopIteration();
+		return self.a
 
-	
-		
-
-    	
-
-
-
+	def __getitem__(self, n):
+		if isinstance(n, int):
+			a, b =1, 1 
+			for x in range(n):
+				a, b = b, a + b
+			return a
+		if isinstance(n, slice):
+			start = n.start
+			stop = n.stop
+			a, b = 1,1
+			L = []
+			for x in range(stop):
+				if x >= start:
+					L.append(a)
+				a, b = b, a + b
+			return L
 
