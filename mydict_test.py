7ce510aed270439519834e129fe04be099a1d5cf
吴祎楠 <wuyinan@crazyhornets.com>
2015-09-14 13:03:05 +0800

    test() update-80%

--- mydict_test.py
+++ mydict_test.py
@@ -9,6 +9,33 @@ class TestDict(unittest.TestCase):
 	def test_init(self):
 		d = Dict(a = 1, b = 'test')
 		self.assertEquals(d.a, 1)
-		pass
+		self.assertEquals(d.b, 'test')
+		self.assertTrue(isinstance(d, dict))
+
+	def test_key(self):
+		d = Dict()
+		d['key'] = 'value'
+		self.assertEquals(d.key, 'value')
+
+	def test_attr(self):
+		d = Dict()
+		d.key = 'value'
+		self.assertTrue('key' in d)
+		self.assertEquals(d['key'], 'value')
+
+	def test_keyerror(self):
+		d = Dict()
+		with self.assertRaises(keyError):
+			value = d['empty']
+
+	def test_attrerror(self):
+		d = Dict()
+		with self.assertRaises(AttributeError):
+			value = d.empty
+	
+	if __name__ =='__main__':
+		unittest.main()
+		
+
 
 		
