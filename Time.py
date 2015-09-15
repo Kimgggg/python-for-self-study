af74574e2a30c7a39ff9607eb1a43deeca90086f
吴祎楠 <wuyinan@crazyhornets.com>
2015-09-15 14:20:03 +0800

    GUI add

--- Time.py
+++ Time.py
@@ -2,14 +2,12 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-import datetime
+import datetime, time
 
-now = datetime.datetime.now()
+now = time.strftime('%Y-%m-%d %H:%M:%S')
+print now
 
 d1 = now - datetime.timedelta(hours = 1)
-print d1.strftime('%y-%m-%d %H:%S:%M')
-
-d2 = now - datetime.timedelta(days = 1)
-print d2.strftime('%Y-%m-%d %H:%S:%M') 
+print d1.strftime('%Y-%m-%d %H:%S:%M')
 
 #bug未修复
