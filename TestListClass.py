d23b8b41e322a7729b804f143d993367d5fd44ba
吴祎楠 <wuyinan@crazyhornets.com>
2015-09-28 17:56:20 +0800

    test bug

--- TestListClass.py
+++ TestListClass.py
@@ -49,7 +49,29 @@ class Permutations(object):
 	def len():
 		pass
 
+'''
+黑哥示例
+'''
+def cobmo2ls(ls1,ls2,concat):
+	for i in range(0,len(ls1)):
+		for j in range(0,len(ls2)):
+			yield concat(ls1[i],ls2[j])
 
+ls1 = ['我','你','他']
+ls2 = ['和','不',' 能']
+
+def concatStr(a,b):
+	return a + b
+for item in cobmo2ls(ls1,ls2,concatStr):
+	print item
+
+# ls1 = []
+# Continue_ls1 = 0
+# while Continue_ls1 == 0:
+# 	ls1.append(raw_input('请继续输入条件步骤:\n'))
+# 	Continue_ls1 = input('是否继续输入？继续输入请按数字“0”,按其他“数字”键退出\n')
+
+# print base64.b64encode(ls1)
 
 
 		
