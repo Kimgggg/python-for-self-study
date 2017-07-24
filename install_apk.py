ea1b4952c7537d7d8b5f755b41bc4aed6ce72096
wuyinan <180551045@qq.com>
2017-07-24 22:11:34 +0800

    update

--- install_apk.py
+++ install_apk.py
@@ -14,21 +14,21 @@ NUM = 0
 dirdir = os.listdir(PATH)
 
 for find_apk in dirdir:
-	if find_apk.split(".")[-1] == WORD:
-		APK_NAME.append(find_apk)
-	else:
-		pass
+    if find_apk.split(".")[-1] == WORD:
+        APK_NAME.append(find_apk)
+    else:
+        pass
 
 if APK_NAME == []:
-	print "没有找到安装包"
-	sys.exit()
+    print "没有找到安装包"
+    sys.exit()
 
 for y in APK_NAME:
-	CreateTime = ""
-	os.path.getctime(PATH + "/" + y)
-	timeTuple = time.localtime(os.path.getctime(PATH + "/" + y))
-	CreateTime = time.strftime("%Y-%m-%d %H:%M:%S", timeTuple)
-	NUM = NUM + 1
-	print str(NUM) + ". " + y + "    创建于" + CreateTime	
+    CreateTime = ""
+    os.path.getctime(PATH + "/" + y)
+    timeTuple = time.localtime(os.path.getctime(PATH + "/" + y))
+    CreateTime = time.strftime("%Y-%m-%d %H:%M:%S", timeTuple)
+    NUM = NUM + 1
+    print str(NUM) + ". " + y + "    创建于" + CreateTime
 install_apk = input("请输入要安装的apk序号：\n")
 os.system("adb install -r " + PATH + "/" + APK_NAME[install_apk - 1])
