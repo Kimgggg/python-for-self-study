6ec909d865d7a1885ae206e09837fa05a33be73f
wuyinan <wuyinan@playcrab.com>
2017-08-11 15:26:35 +0800

    update

--- wyn.py
+++ wyn.py
@@ -10,18 +10,14 @@ import hashlib
 
 # execfile('/data/work/pctools/tool.py')
 
-
-
-
-
 fileNameList = []
 fileNameList_data = []
 path_svn = "/data/work/svn/数据表/数值开发/"
 path_localData = "/Users/playcrab/Documents/kof_debug_excel/"
 path_diff = "/Users/playcrab/Documents/debug_diff/"
-dict_one = {}
-dict_data = {}
-count_a = 0
+dict_svn = {}
+dict_local = {}
+scaler = 0
 
 shutil.rmtree(path_diff)
 os.mkdir(path_diff)
@@ -32,66 +28,47 @@ def svn_update(svn_path):
 	os.system("svn update")
 	os.chdir(current_Path)
 
-
-def getHash(filename, path, dict_name):
+def getHashCode(filename, path, dict_name):
 	with open(path + filename, 'rb') as f:
 		sha1obj = hashlib.sha1()
 		sha1obj.update(f.read())
 		hash = sha1obj.hexdigest()
-		# print filename + "的hash   是   " + hash
-		# list_name.append(hash)
 		dict_name[filename] = hash
+
+def copyFile(svn_path, filename, targetPath):
+	os.system("cp " + svn_path + filename + " " + targetPath)
+	print "正在往" + targetPath + "复制" + filename
 
 svn_update(path_svn)
 fileNameList = os.listdir(path_svn)
 fileNameList_data = os.listdir(path_localData)
 
 for x in fileNameList:
-	getHash(x, path_svn, dict_one)
+	getHashCode(x, path_svn, dict_svn)
 
 for y in fileNameList_data:
-	getHash(y, path_localData, dict_data)
+	getHashCode(y, path_localData, dict_local)
 
-for key in dict_one:
-	for key_data in dict_data:
+for key in dict_svn:
+	for key_data in dict_local:
 		if key == key_data:
-			#print "文件名一致"
-			if dict_one[key] == dict_data[key_data]:
-				print dict_one[key],"和",dict_data[key_data],"hash一样"
+			if dict_svn[key] == dict_local[key_data]:
+				pass
 			else:
-				print key," need going copy"
-				os.system("cp " + path_svn + key + " " + path_diff)
-				count_a = count_a + 1
-				print ">>>>>>>>>>>>>>>",count_a 
-		elif key not in dict_data:
-			os.system("cp " + path_svn + key + " " + path_diff)
-			count_a = count_a + 1
-			print ">>>>>>>>>>>>>>>",count_a 
+				copyFile(path_svn, key, path_diff)
+				scaler += 1
+		elif key not in dict_local:
+			copyFile(path_svn, key, path_diff)
+			scaler += 1
 		else:
 			pass
 
 shutil.rmtree(path_localData)
 shutil.copytree(path_svn,path_localData)
-if count_a != 0:
-	os.system("python inspect_to_excel_debug.py")
+if scaler != 0:
+	print "先不执行脚本>>>>>>>>>>>>>>>>>>>>>>>>>"
+	#execfile("inspect_to_excel_debug.py")
 else:
 	pass
 
-
-
-
-# for key in dict_one:
-# 	print key, '>>>>>>>>>>>' , dict_one[key]
-
-# for key in dict_data:
-# 	print key, "<<<<<<<<<<<", dict_data[key]
-
-print len(dict_one),">>>>>>>",len(dict_data)
-
-# print fileNameList.index("WorldBossEnemy.xlsx")
-
-# print fileNameList[fileNameList.index("WorldBossEnemy.xlsx")]
-
-# for name in fileNameList:
-# 	print fileNameList[fileNameList.index(name)] + "的下标是: " + str(fileNameList.index(name))
 
