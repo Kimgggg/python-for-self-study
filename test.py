83a5ab9b2d42514d66e222789b3bb3b552b22ed3
wuyinan <wuyinan@playcrab.com>
2017-09-20 00:52:44 +0800

    暂时提交一版 add 自动获取debug的html add 自动解析html，查找xpath id

--- test.py
+++ test.py
@@ -4,51 +4,94 @@
 
 from selenium.webdriver.support.ui import Select
 from selenium import webdriver
+from sgmllib import SGMLParser
 import time
 import os
 import sys
 import other_config
+import urllib2
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-# user_rid = raw_input("please input rid:\n")
-# print '''
-# 1.开发机1
-# 2.开发机2
-# 3.开发机3
-# 4.dev3
-# 5.dev4
-# '''
-def translate_xpath(xpath):
-    xpath = xpath.replace('"', "'")
-    return driver.find_element_by_xpath(xpath)
-
-# if sys.platform == 'win32':
-# 	filePath = filePath.decode('gbk')
-# 	test = u"".join(atext.decode("gbk"))
-# 	test = u"".join(atext.decode("gbk"))
-
-
-driver = webdriver.Firefox()
-driver.get("http://120.26.4.254/projects/war/issues/new")
-translate_xpath("//*[@id='username']").send_keys("wuyinan")
-translate_xpath("//*[@id='password']").send_keys("12345678")
-translate_xpath("//*[@id='login-form']/form/table/tbody/tr[4]/td[2]/input").click()
-time.sleep(1)
-select = Select(driver.find_element_by_name("issue[tracker_id]"))
-select.select_by_value('2')
-
-atext = raw_input("please input project name:\n")
-test = u"".join(atext.decode("gbk"))
-translate_xpath("//*[@id='issue_subject']").send_keys(test)
 
 
 
+# import urllib2
+# response = urllib2.urlopen("http://192.168.5.207:8001/demo/ctrl.php")
+# html = response.read()
+# # print html.decode('utf-8').encode('gbk')
+# print type(html)
+# print html
 
-time.sleep(1)
-
-# drop_down.find_element_by_xpath("//*[@id='menu'][contains(text(),'添加资源')]").click()
-# # select = Select(driver.find_element_by_xpath("//*[@id='left']"))
-# # print select
-
-# # select.select_by_value("[Tools]添加资源")
+file_for_path = open('D:/wyn/123.txt')
+for (num, value) in enumerate(file_for_path):
+    if '<ul id="menu">' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        first_num = num
+    elif '[Tools]添加资源' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Tools]玩家升级' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Tools]设置Vip等级' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Team]升级潜能' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Tools]主线精英重置到某一副本补差删多' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Tools]个人物品发放' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Tools]个人物品发放' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Treasure]进阶散件宝物' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Treasure]进阶组合宝物' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Tools]发放英雄' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Hero]英雄升星' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Tools]个人物品发放' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Hero]技能升级' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Tools]发放兵团' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Team]怪兽方阵升级' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Team]符文批量升级（装备）' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Team]怪兽方阵符文升阶' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Team]怪兽方阵升大星' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Team]怪兽方阵升小星' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Team]激活潜能' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Tools]重置PVE玩法次数' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk')
+        print (num - first_num)/2
+    elif '[Team]怪兽方阵进阶' in value:
+        print "line number", num, "is:", value.decode('utf-8').encode('gbk') 
+        print (num - first_num)/2       
+file_for_path.close()
