e965b5da9e9833d1693e6eb8e1eaf97f1b694771
wuyinan <wuyinan@playcrab.com>
2017-09-27 12:16:25 +0800

    'test'

--- find_svn_version.py
+++ find_svn_version.py
@@ -4,23 +4,47 @@
 
 import sys
 import os
-import pprint
 import svn.local
 import re
+import shutil
 
 
-current_path = os.getcwd()
-os.chdir("E:\war\svn\configCsv\csv")
+csv_develop = "E:\war\svn\configCsv\csv"
+csv_preRelease = "E:\war\svn\configCsv_test\csv"
+csv_Release = "E:\war\svn\configCsv_walle_china\csv"
+csv_compare = "D:\csv_compare"
 
-a = os.popen('svn log -l 10')
-b = a.read()
-print "<<<<<<<<>>>>>>>>>>" + b + ">>>>>>>>>>>>>>>>>>>>>>"
-c = re.findall(r'[0-9][0-9][0-9][0-9][0-9]',b) # c =re.findall(r"r(\d\d\d\d\d)",b)
-c.sort()
+# current_path = os.getcwd()
+# os.chdir("E:\war\svn\configCsv\csv")
 
-for x in c:
-	os.system("svn update -r " + str(x))
-	os.system("git status")
-	os.system("git add . & git ci -m '" + str(x) + "'")
+# svn_version = os.popen('svn log -l 10')
+# svn_version_list = svn_version.read()
+# # print "<<<<<<<<>>>>>>>>>>" + svn_version_list + ">>>>>>>>>>>>>>>>>>>>>>"
+# find_version = re.findall(r'[0-9][0-9][0-9][0-9][0-9]',svn_version_list) # find_version =re.findall(r"r(\d\d\d\d\d)",svn_version_list)
+# find_version.sort()
 
-os.system("svn update")
+# for x in find_version:
+# 	os.system("svn update -r " + str(x))
+# 	os.system("git st")
+# 	os.system("git add . & git ci -m '" + str(x) + "'")
+
+# os.system("svn update")
+# os.chdir(current_path)
+
+def del_files(path, filetype):
+	#传入路径与需要删除的文件类型“.csv”
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if name.endswith(filetype):
+                os.remove(os.path.join(root, name))
+  	print ("Delete File: " + os.path.join(root, name))
+
+
+del_files(csv_compare, ".csv")
+os.system("xcopy " + csv_preRelease + " " + csv_compare + " /Y")
+os.system("git add . & git ci -m 'test'")
+del_files(csv_compare, ".csv")
+os.system("xcopy " + csv_develop + " " + csv_compare + " /Y")
+os.system("git add . & git ci -m 'test2'")
+
+
